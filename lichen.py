1146b77b83c842bd51ed79bd1c99eafc0bc96733
janbridley <bradleyjenj@gmail.com>
2026-01-07 11:39:59 -0500

    Clean up and invert

--- lichen.py
+++ lichen.py
@@ -1,5 +1,5 @@
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 
 # --- Configuration ---
 # WIDTH = 768
@@ -8,7 +8,7 @@ WIDTH = 320
 HEIGHT = 200
 TOTAL_PIXELS = WIDTH * HEIGHT
 FPS = 20
-STEPS_PER_FRAME = 4000
+STEPS_PER_FRAME = 16000
 # STEPS_PER_FRAME = 64000
 
 MAX_BRIGHTNESS = 0x1E  # 30 (The 'spore' brightness)
@@ -26,7 +26,7 @@ center_idx = (HEIGHT // 2) * WIDTH + (WIDTH // 2)
 image_buffer[center_idx] = MAX_BRIGHTNESS
 
 frames = []
-total_frames = FPS * 30  # 30 seconds at FPS
+total_frames = FPS * 60  # 30 seconds at FPS
 
 print("Generating frames...")
 for frame_num in range(total_frames):
@@ -35,15 +35,15 @@ for frame_num in range(total_frames):
         # "esi += 0x5A6A6D6C"
         # "rotate the bits of esi right once"
         # "if si < 0xFA00, return... else call pRNG(esi)"
+        idx = 999999999
+        while idx >= TOTAL_PIXELS:
+            # integer add with 32-bit wrap
+            esi = (esi + MAGIC_CONST) & 0xFFFFFFFF
 
-        # integer add with 32-bit wrap
-        esi = (esi + MAGIC_CONST) & 0xFFFFFFFF
+            # ROR 1 (Rotate Right 1 bit)
+            esi = ((esi >> 1) | (esi << 31)) & 0xFFFFFFFF
 
-        # ROR 1 (Rotate Right 1 bit)
-        esi = ((esi >> 1) | (esi << 31)) & 0xFFFFFFFF
-
-        # Use full 32 bits for indexing to support larger resolutions
-        idx = esi % TOTAL_PIXELS
+            idx = esi & 0xFFFF
 
         px = image_buffer[idx]
         if px == 0:
@@ -69,10 +69,8 @@ for frame_num in range(total_frames):
     # bitmapData[i] = val ? (uint8_t)((val * 255) / kMaxBrightness) : 0;
     # scaled = (image_buffer * 255 / MAX_BRIGHTNESS).astype(np.uint8)
     print(image_buffer.min(), image_buffer.max())
-    frame_array = np.where(image_buffer > 0, image_buffer * 255, 0).reshape(
-        HEIGHT, WIDTH
-    )
-    frames.append(Image.fromarray(frame_array, mode="L"))
+    frame_array = np.where(image_buffer > 0, image_buffer * 8, 0).reshape(HEIGHT, WIDTH)
+    frames.append(ImageOps.invert(Image.fromarray(frame_array, mode="L")))
 
 print(f"Saving GIF...")
 frames[0].save(
